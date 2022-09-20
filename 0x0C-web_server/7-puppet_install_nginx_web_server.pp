7f49bd23637511e96ac97a7198128583deb68c17
MikeOdhiambo <odhiambomike413@gmail.com>
2022-09-21 02:09:37 +0300

    Advanced task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
-# Puppet automate creating a custom HTTP header response.
+# automate creating custom HTTP header response with Puppet.
 
 exec { 'http config':
   provider => shell,
-  command  => 'sudo apt-get update -y && sudo apt-get install -y nginx && echo "Hello World!" > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;' /etc/nginx/sites-available/default && echo "Ceci n'est pas une page" > /usr/share/nginx/html/custom_404.html && sed -i '/listen 80 default_server;/a error_page 404 /custom_404.html;\nlocation = /custom_404.html {\nroot /usr/share/nginx/html; \ninternal;\n}' /etc/nginx/sites-available/default && sudo service nginx restart',
+  command  => 'sudo apt-get update -y && sudo apt-get install -y nginx && echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i "19i rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;" /etc/nginx/sites-enabled/default && echo "Ceci n\'est pas une page" | sudo tee /usr/share/nginx/html/page_error_404.html && sudo sed -i "37i error_page 404 /page_error_404.html;\nlocation = /page_error_404.html {\nroot /usr/share/nginx/html; \ninternal;\n}" /etc/nginx/sites-enabled/default && sudo service nginx restart',
 }
