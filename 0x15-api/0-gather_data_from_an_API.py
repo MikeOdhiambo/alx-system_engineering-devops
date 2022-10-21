90855a0eb8f18beeda3d25095125723ba5dc4588
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-22 02:04:28 +0300

    Use get to access dictionary elements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """gather_data_from_an_API- Gathers users task data from an API"""
-import json
-import requests
-from sys import argv
 
-userId = int(argv[1])
-userReq = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                       format(userId))
-todosReq = requests.get("https://jsonplaceholder.typicode.com/todos")
-userJson = userReq.json()
-todosJson = todosReq.json()
-inc = []
-comp = []
-for item in todosJson:
-    if item["userId"] == userId:
-        if item["completed"]:
-            comp.append(item)
-        else:
-            inc.append(item)
-print("Employee {} is done with tasks({}/{}):".
-      format(userJson["name"], len(comp), (len(comp) + len(inc))))
-for task in comp:
-    print("\t {}".format(task["title"]))
+
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
+
+    userId = int(argv[1])
+    userReq = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                           format(userId))
+    todosReq = requests.get("https://jsonplaceholder.typicode.com/todos")
+    userJson = userReq.json()
+    todosJson = todosReq.json()
+    inc = []
+    comp = []
+    for item in todosJson:
+        if item.get("userId") == userId:
+            if item.get("completed"):
+                comp.append(item)
+            else:
+                inc.append(item)
+    print("Employee {} is done with tasks({}/{}):".
+          format(userJson.get("name"), len(comp), (len(comp) + len(inc))))
+    for task in comp:
+        print("\t {}".format(task.get("title")))
