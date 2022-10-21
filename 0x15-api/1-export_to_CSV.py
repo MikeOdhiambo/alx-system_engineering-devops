b3a4a1901e9bb65eae364ad5b6479d5642092b6a
MikeOdhiambo <odhiambomike413@gmail.com>
2022-10-22 02:06:38 +0300

    Use get to access dictionary elements

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,15 +10,15 @@ userReq = requests.get("https://jsonplaceholder.typicode.com/users/{}".
 todosReq = requests.get("https://jsonplaceholder.typicode.com/todos")
 userJson = userReq.json()
 todosJson = todosReq.json()
-userName = userJson["username"]
+userName = userJson.get("username")
 tasks = []
 for item in todosJson:
-    if item["userId"] == userId:
+    if item.get("userId") == userId:
         tasks.append(item)
 with open("USER_ID.csv", mode="w") as csv_file:
     fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     for task in tasks:
         writer.writerow({"USER_ID": userId, "USERNAME": userName,
-                         "TASK_COMPLETED_STATUS": task["completed"],
-                         "TASK_TITLE": task["title"]})
+                         "TASK_COMPLETED_STATUS": task.get("completed"),
+                         "TASK_TITLE": task.get("title")})
